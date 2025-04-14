89a12ce5082c34052c4e667d6531f5c244960fa2
leodinelli <leodinelli2007@gmail.com>
2025-04-14 07:05:45 -0300

    teste

--- 20250410/JogoDaVelha/JogaJogoDaVelha.java
+++ 20250410/JogoDaVelha/JogaJogoDaVelha.java
@@ -1,13 +1,49 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class JogaJogoDaVelha {
     public static void main(String[] args) {
-        System.out.println("Jogando até ganhar");
-        /** Sua classe deve fazer o seguinte:
-         * Instanciar um jogo da velha
-         * Fazer jogadas até ganhar ou empatar
-         * Imprimir qual jogador ganhou ou se houve empate
-         * Imprimir o tabuleiro final
-         * Após a impressão do tabuleiro, deve ser oferecida
-         * a opção de iniciar um novo jogo
-         */
+        Scanner entrada = new Scanner(System.in);
+        Random aleatorio = new Random();
+        boolean jogarDeNovo = true;
+
+        while (jogarDeNovo) {
+            JogoDaVelha jogo = new JogoDaVelha();
+
+            while (jogo.vencedor() == 0) {
+                int linha = aleatorio.nextInt(3);   
+                int coluna = aleatorio.nextInt(3);
+
+                try {
+                    jogo.poePeca(linha, coluna); // tenta jogar
+                } catch (Exception e) {
+                    // posição ocupada
+                }
+            }
+
+            // tabuleiro final
+            System.out.println("Tabuleiro final:");
+            System.out.println(jogo.toString());
+
+        
+            int resultado = jogo.vencedor();
+            if (resultado == JogoDaVelha.X) {
+                System.out.println("O jogador X venceu!");
+            } else if (resultado == JogoDaVelha.O) {
+                System.out.println("O jogador O venceu!");
+            } else {
+                System.out.println("O jogo terminou em empate!");
+            }
+
+            // jogar de novo
+            System.out.print("Deseja jogar novamente? (s/n): ");
+            String resposta = entrada.next();
+            if (!resposta.equalsIgnoreCase("s")) {
+                jogarDeNovo = false;
+                System.out.println("Obrigado por jogar!");
+            }
+        }
+
+        entrada.close();
     }
 }
--- 20250410/JogoDaVelha/TesteJogoDaVelha.java
+++ 20250410/JogoDaVelha/TesteJogoDaVelha.java
@@ -1,13 +1,49 @@
 public class TesteJogoDaVelha {
     public static void main(String[] args) {
-        System.out.println("Jogando para ganhar");
-        // Instanciando o jogo da velha
+        
+        System.out.println("Jogo da velha:");
         JogoDaVelha jogo = new JogoDaVelha();
-        jogo.poePeca(1,1);
-        jogo.poePeca(1,2);
-        jogo.poePeca(2,1);
-        jogo.poePeca(0,1);
-        String primeiroJogo = jogo.toString();
-        System.out.println(primeiroJogo);
-   }
+        
+        /*teste do X vencer  
+        jogo.poePeca(0, 0); 
+        jogo.poePeca(0, 1); 
+        jogo.poePeca(1, 1); 
+        jogo.poePeca(0, 2); 
+        jogo.poePeca(2, 2); 
+
+        /*teste 0 vencer
+        jogo.poePeca(1, 1);
+        jogo.poePeca(0, 0);
+        jogo.poePeca(1, 2);
+        jogo.poePeca(0, 1);
+        jogo.poePeca(2, 1);
+        jogo.poePeca(0, 2);*/
+
+        //teste empate
+        jogo.poePeca(0, 0);
+        jogo.poePeca(0, 1);
+        jogo.poePeca(1, 1);
+        jogo.poePeca(1, 0);
+        jogo.poePeca(2, 0);
+        jogo.poePeca(2, 2);
+        jogo.poePeca(2, 1);
+        jogo.poePeca(0, 2);
+        jogo.poePeca(1, 2);
+
+
+
+        System.out.println(jogo.toString());
+        mostrarVencedor(jogo);    }
+
+        //mostrar o vencedor daora
+        private static void mostrarVencedor(JogoDaVelha jogo) {
+            int resultado = jogo.vencedor();
+        if (resultado == JogoDaVelha.X) {
+            System.out.println("O jogador X venceu!");
+        } else if (resultado == JogoDaVelha.O) {
+            System.out.println("O jogador O venceu!");
+        } else {
+            System.out.println("O jogo terminou em empate!");
+        }
+    }
 }
